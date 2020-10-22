3c91f0d79532b13dc98bee5d574ff531f95ff142
Mateusz <matkorytowski@gmial.com>
2020-10-22 18:57:53 +0200

    ER # assign variables in one loop

--- src/java/chapter/c/number/control/RandomNumberGenerator.java
+++ src/java/chapter/c/number/control/RandomNumberGenerator.java
@@ -12,5 +12,4 @@ public class RandomNumberGenerator {
                 .boxed()
                 .collect(Collectors.toList());
     }
-
 }
--- src/java/chapter/c/task/f/for_/NumberOperationFor.java
+++ src/java/chapter/c/task/f/for_/NumberOperationFor.java
@@ -7,55 +7,41 @@ import chapter.c.task.f.NumberOperation;
 
 public class NumberOperationFor implements NumberOperation {
 
-    private final int min;
-    private final int max;
-    private final double average;
+    private int min;
+    private int max;
+    private double average;
     private final List<Integer> scope;
 
     private NumberOperationFor(Builder builder) {
         Objects.requireNonNull(builder.scope);
-
         scope = builder.scope;
+        calculateParams(scope);
+    }
 
-        average = calculateAverage(scope);
+    private void calculateParams(List<Integer> scope) {
+        int currentMin = scope.get(0);
+        int currentMax = scope.get(0);
+        int sum = 0;
 
-        this.min = calculateMin(scope);
-        this.max = calculateMax(scope);
+        for (int i = 0; i < scope.size(); i++) {
+            int currentNumber = scope.get(i);
+            if (currentMin > currentNumber) {
+                currentMin = currentNumber;
+            }
+            if (currentMax < currentNumber) {
+                currentMax = currentNumber;
+            }
+
+            sum += currentNumber;
+        }
+
+        this.average = (double) sum / (double) scope.size();
+        this.min = currentMin;
+        this.max = currentMax;
     }
 
     public static Builder builder() {
         return new Builder();
-    }
-
-    private Integer calculateMin(List<Integer> numbers) {
-        int currentMin = numbers.get(0);
-        for (int i = 1; i < scope.size(); i++) {
-            if (currentMin > scope.get(i)) {
-                currentMin = scope.get(i);
-            }
-        }
-
-        return currentMin;
-    }
-
-    private Integer calculateMax(List<Integer> numbers) {
-        int currentMax = numbers.get(0);
-        for (int i = 1; i < scope.size(); i++) {
-            if (currentMax < scope.get(i)) {
-                currentMax = scope.get(i);
-            }
-        }
-
-        return currentMax;
-    }
-
-    private double calculateAverage(List<Integer> scope) {
-        int sum = 0;
-        for (int i = 0; i < scope.size(); i++) {
-            sum += scope.get(i);
-        }
-
-        return (double) sum / (double) scope.size();
     }
 
     @Override
